0601e5715c539636b28eb9ffca275cdc1aa74db5
pat <patrickfungdesigns@gmail.com>
2023-10-29 15:23:41 +1100

    added new levels admending script to objective

--- Bub/Assets/Scripts/ObjectMatch.cs
+++ Bub/Assets/Scripts/ObjectMatch.cs
@@ -5,25 +5,33 @@ using UnityEngine;
 
 public class ObjectMatch : MonoBehaviour
 {
-    
 
-
-
-
+   public int FinalScore; 
+   public int CurrentScore; 
 
     void Start()
     {
-        
+        //"Collect x amount of numbers to pass
+        CurrentScore = 0; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(CurrentScore);
+
+        if (FinalScore == CurrentScore)
+        {
+            Debug.Log("Play Success Music, Play Star Particle Effect. Well Done"); 
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-
+        if(collision.tag == "Apple")
+        {
+            Destroy(collision.gameObject);
+            CurrentScore ++; 
+        }
     }
 }
